b45e75c82721bc64395b7521f7d70905e55dd2e0
redmmm <108681348+redmmm@users.noreply.github.com>
2026-01-08 11:46:24 +0100

    Fix GitHub Pages deployment: HashRouter + base config

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import ChannelDashboard from "./pages/ChannelDashboard";
 import NewsPage from "./pages/NewsPage";
@@ -19,7 +19,7 @@ const App = () => (
       <div className="dark">
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/channel/:slug" element={<ChannelDashboard />} />
@@ -30,7 +30,7 @@ const App = () => (
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </div>
     </TooltipProvider>
   </QueryClientProvider>
